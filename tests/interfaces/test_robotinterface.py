3239d7f001b8faff9544fa77d2965986d9ca7c08
aestene <arnts@equinor.com>
2021-10-15 15:15:18 +0200

    Fix test for robot interface

--- tests/interfaces/test_robotinterface.py
+++ tests/interfaces/test_robotinterface.py
@@ -3,7 +3,3 @@ from isar_robot import robotinterface
 
 def test_robotinterface():
     robot = robotinterface.Robot()
-
-    scheduler = robot.scheduler
-    storage = robot.storage
-    telemetry = robot.telemetry
