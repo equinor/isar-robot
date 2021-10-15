179015b62e3f6379bcf4334e6b96b298d4d2301d
aestene <arnts@equinor.com>
2021-10-15 12:51:32 +0200

    Adjust interface definitions to match new version

--- src/isar_robot/robotinterface.py
+++ src/isar_robot/robotinterface.py
@@ -1,48 +1,39 @@
-from robot_interfaces.robot_interface import (
-    RobotInterface,
-    RobotSchedulerInterface,
-    RobotStorageInterface,
-    RobotTelemetryInterface,
-)
+from typing import Any, Optional, Sequence, Tuple
+
+from robot_interface.models.geometry.joints import Joints
+from robot_interface.models.geometry.pose import Pose
+from robot_interface.models.inspection.inspection import Inspection, InspectionResult
+from robot_interface.models.mission import MissionStatus, Step
+from robot_interface.robot_interface import RobotInterface
 
 
 class Robot(RobotInterface):
-    def __init__(self):
-        self.scheduler = Scheduler()
-        self.storage = Storage()
-        self.telemetry = Telemetry()
-
-
-class Scheduler(RobotSchedulerInterface):
-    def schedule_step(self):
+    def schedule_step(self, step: Step) -> Tuple[bool, Optional[Any], Optional[Joints]]:
         raise NotImplementedError
 
-    def mission_scheduled(self):
+    def mission_scheduled(self) -> bool:
         raise NotImplementedError
 
-    def mission_status(self):
+    def mission_status(self, mission_id: Any) -> MissionStatus:
         raise NotImplementedError
 
-    def abort_mission(self):
+    def abort_mission(self) -> bool:
         raise NotImplementedError
 
-    def log_status(self):
+    def log_status(
+        self, mission_id: Any, mission_status: MissionStatus, current_step: Step
+    ):
         raise NotImplementedError
 
-
-
-class Storage(RobotStorageInterface):
-    def get_inspection_references(self):
+    def get_inspection_references(
+        self, vendor_mission_id: Any, current_step: Step
+    ) -> Sequence[Inspection]:
         raise NotImplementedError
 
-    def download_inspection_result(self):
+    def download_inspection_result(
+        self, inspection: Inspection
+    ) -> Optional[InspectionResult]:
         raise NotImplementedError
 
-
-
-class Telemetry(RobotTelemetryInterface):
-    def robot_pose(self):
-        raise NotImplementedError
-
-    def robot_joints(self):
+    def robot_pose(self) -> Pose:
         raise NotImplementedError
