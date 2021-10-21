87922bb030b2745685ac03e0a9a9ff581ba04984
Edda Solem <esole@equinor.com>
2021-10-21 10:16:03 +0200

    Add test to verify robotinterface

--- tests/interfaces/test_robotinterface.py
+++ tests/interfaces/test_robotinterface.py
@@ -1,5 +1,22 @@
-from isar_robot import robotinterface
+from robot_interface.robot_interface import RobotInterface
+from isar_robot.robotinterface import Robot
+from typing import Tuple
 
 
 def test_robotinterface():
-    robot = robotinterface.Robot()
+    robot: Robot = Robot()
+
+    for func in dir(RobotInterface):
+        if not callable(getattr(RobotInterface, func)) or func.startswith("__"):
+            continue
+
+        arg_robot: int = getattr(Robot, func).__code__.co_argcount
+        arg_isar: int = getattr(RobotInterface, func).__code__.co_argcount
+
+        args_robot: Tuple[str] = getattr(Robot, func).__code__.co_varnames[:arg_robot]
+        args_isar: Tuple[str] = getattr(RobotInterface, func).__code__.co_varnames[
+            :arg_isar
+        ]
+
+        assert arg_robot == arg_isar
+        assert args_robot == args_isar
