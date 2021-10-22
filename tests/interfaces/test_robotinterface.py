c4ed70b2c7bb92307eb186c7cd7a847c1ae6ca74
Edda Solem <esole@equinor.com>
2021-10-22 10:39:26 +0200

    Move generic interface test to isar

--- tests/interfaces/test_robotinterface.py
+++ tests/interfaces/test_robotinterface.py
@@ -1,22 +1,6 @@
-from robot_interface.robot_interface import RobotInterface
 from isar_robot.robotinterface import Robot
-from typing import Tuple
+from robot_interface.test_robot_interface import interface_test
 
 
 def test_robotinterface():
-    robot: Robot = Robot()
-
-    for func in dir(RobotInterface):
-        if not callable(getattr(RobotInterface, func)) or func.startswith("__"):
-            continue
-
-        arg_robot: int = getattr(Robot, func).__code__.co_argcount
-        arg_isar: int = getattr(RobotInterface, func).__code__.co_argcount
-
-        args_robot: Tuple[str] = getattr(Robot, func).__code__.co_varnames[:arg_robot]
-        args_isar: Tuple[str] = getattr(RobotInterface, func).__code__.co_varnames[
-            :arg_isar
-        ]
-
-        assert arg_robot == arg_isar
-        assert args_robot == args_isar
+    interface_test(Robot())
