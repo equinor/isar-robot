9554b67bdffc0c2ed24f22222db32683208c9ca6
Anette Uttisrud <autt@equinor.com>
2021-11-22 16:29:51 +0100

    Update author email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description_content_type="text/markdown",
     version="1.0.2",
     author="Equinor ASA",
-    author_email="fg_robots@equinor.com",
+    author_email="fg_robots_dev@equinor.com",
     url="https://github.com/equinor/isar_robot",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
