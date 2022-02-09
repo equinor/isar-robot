ef480bc32f44cbb3dd173625a4ac92f78cd17250
aestene <arnts@equinor.com>
2022-02-09 14:29:48 +0100

    Include example images in package

--- setup.py
+++ setup.py
@@ -11,6 +11,12 @@ setup(
     url="https://github.com/equinor/isar_robot",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    package_data={
+        "isar_robot": [
+            "example_images/for-science-you-monster.jpg",
+            "example_images/wheatley-remain-calm.jpg",
+        ]
+    },
     classifiers=[
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
