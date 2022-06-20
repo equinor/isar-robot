c9db8da86a3ff6cede291852cb20ede724adb30c
aestene <arnts@equinor.com>
2022-06-20 10:06:30 +0200

    Add telemetry publishing support

--- src/isar_robot/robotinterface.py
+++ src/isar_robot/robotinterface.py
@@ -1,10 +1,13 @@
+import json
 import logging
 import os
 import random
 from datetime import datetime
 from logging import Logger
 from pathlib import Path
-from typing import Sequence
+from queue import Queue
+from threading import Thread
+from typing import List, Sequence
 
 from alitra import Frame, Orientation, Pose, Position
 from robot_interface.models.initialize import InitializeParams
@@ -16,6 +19,12 @@ from robot_interface.models.inspection.inspection import (
 )
 from robot_interface.models.mission import InspectionStep, Step, StepStatus
 from robot_interface.robot_interface import RobotInterface
+from robot_interface.telemetry.mqtt_client import MqttTelemetryPublisher
+from robot_interface.telemetry.payloads import (
+    TelemetryBatteryPayload,
+    TelemetryPosePayload,
+)
+from robot_interface.utilities.json_service import EnhancedJSONEncoder
 
 
 class Robot(RobotInterface):
@@ -65,3 +74,50 @@ class Robot(RobotInterface):
 
     def initialize(self, params: InitializeParams) -> None:
         return
+
+    def get_telemetry_publishers(self, queue: Queue, robot_id: str) -> List[Thread]:
+        publisher_threads: List[Thread] = []
+
+        pose_publisher: MqttTelemetryPublisher = MqttTelemetryPublisher(
+            mqtt_queue=queue,
+            telemetry_method=self._get_pose_telemetry,
+            topic=f"isar/{robot_id}/pose",
+            interval=1,
+            retain=True,
+        )
+        pose_thread: Thread = Thread(
+            target=pose_publisher.run,
+            args=[robot_id],
+            name="ISAR Robot Pose Publisher",
+            daemon=True,
+        )
+        publisher_threads.append(pose_thread)
+
+        battery_publisher: MqttTelemetryPublisher = MqttTelemetryPublisher(
+            mqtt_queue=queue,
+            telemetry_method=self._get_battery_telemetry,
+            topic=f"isar/{robot_id}/battery",
+            interval=1,
+            retain=True,
+        )
+        battery_thread: Thread = Thread(
+            target=battery_publisher.run,
+            args=[robot_id],
+            name="ISAR Robot Battery Publisher",
+            daemon=True,
+        )
+        publisher_threads.append(battery_thread)
+
+        return publisher_threads
+
+    def _get_pose_telemetry(self, robot_id: str) -> str:
+        pose_payload: TelemetryPosePayload = TelemetryPosePayload(
+            pose=self.pose, robot_id=robot_id, timestamp=datetime.utcnow()
+        )
+        return json.dumps(pose_payload, cls=EnhancedJSONEncoder)
+
+    def _get_battery_telemetry(self, robot_id: str) -> str:
+        battery_payload: TelemetryBatteryPayload = TelemetryBatteryPayload(
+            battery_level=95.2, robot_id=robot_id, timestamp=datetime.utcnow()
+        )
+        return json.dumps(battery_payload, cls=EnhancedJSONEncoder)
