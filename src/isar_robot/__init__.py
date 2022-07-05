bc392531ee4df4862b41809b28dc741c399cb215
Thomas Sundvoll <thomas@sundvoll.net>
2022-07-05 15:12:51 +0200

    Add version attribute to package

--- src/isar_robot/__init__.py
+++ src/isar_robot/__init__.py
@@ -1,1 +1,8 @@
+from pkg_resources import DistributionNotFound, get_distribution
+
 from .robotinterface import Robot
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass  # package is not installed
