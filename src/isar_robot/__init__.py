6e8fa3e2e35ac064ad8cf973305a2e6df89f298e
Thomas Sundvoll <thomas@sundvoll.net>
2023-12-19 23:46:44 +0100

    Migrate from setup.py to pyproject.toml

--- src/isar_robot/__init__.py
+++ src/isar_robot/__init__.py
@@ -1,8 +1,6 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
 from .robotinterface import Robot
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    pass  # package is not installed
+    from isar_robot.version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
