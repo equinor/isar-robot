a6cbe1ebc78e287282f5ed1aec24f2023eb49fe4
haakonsf <hf@equinor.com>
2024-03-21 09:39:50 +0100

    Optional fail on localization mission

--- src/isar_robot/config/settings.py
+++ src/isar_robot/config/settings.py
@@ -1,4 +1,5 @@
 import importlib.resources as pkg_resources
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -14,8 +15,10 @@ class Settings(BaseSettings):
 
     STEP_DURATION_IN_SECONDS: float = Field(default=5.0)
     MISSION_DURATION_IN_SECONDS: float = Field(default=5.0)
-    SHOULD_FAIL_MISSION: bool = Field(default=False)
-    SHOULD_FAIL_STEP: bool = Field(default=False)
+    SHOULD_FAIL_NORMAL_MISSION: bool = Field(default=False)
+    SHOULD_FAIL_LOCALIZATION_MISSION: bool = Field(default=False)
+    SHOULD_FAIL_NORMAL_STEP: bool = Field(default=False)
+    SHOULD_FAIL_LOCALIZATION_STEP: bool = Field(default=False)
 
     model_config = SettingsConfigDict(
         env_prefix="ROBOT_",
