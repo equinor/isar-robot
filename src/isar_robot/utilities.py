6c908b51be3f84d136fcd91bea56372735b2205c
Eddasol <esole@equinor.com>
2025-02-05 16:00:46 +0100

    Remove unused functions

--- src/isar_robot/utilities.py
+++ src/isar_robot/utilities.py
@@ -1,26 +1,5 @@
-from robot_interface.models.mission.mission import Mission
 from robot_interface.models.mission.task import Task
-from robot_interface.models.mission.task import Localize, ReturnToHome
-
-
-def is_localization_mission(mission: Mission):
-    if len(mission.tasks) != 1:
-        return False
-    if isinstance(mission.tasks[0], Localize):
-        return True
-    return False
-
-
-def is_localization_task(task: Task):
-    return isinstance(task, Localize)
-
-
-def is_return_to_home_mission(mission: Mission):
-    if len(mission.tasks) != 1:
-        return False
-    if isinstance(mission.tasks[0], ReturnToHome):
-        return True
-    return False
+from robot_interface.models.mission.task import ReturnToHome
 
 
 def is_return_to_home_task(task: Task):
