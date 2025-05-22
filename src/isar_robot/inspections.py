f51979c75b9ccb7d83fc72d779e8bd9dedcc74c2
aestene <arnts@equinor.com>
2025-05-22 12:28:35 +0200

    Add CO2 measurement to get inspection

--- src/isar_robot/inspections.py
+++ src/isar_robot/inspections.py
@@ -2,15 +2,12 @@ import os
 import random
 from datetime import datetime, timezone
 from pathlib import Path
-from typing import Union
-
 from robot_interface.models.exceptions.robot_exceptions import (
     RobotRetrieveInspectionException,
 )
 from robot_interface.models.inspection.inspection import (
     Audio,
     AudioMetadata,
-    GasMeasurement,
     GasMeasurementMetadata,
     Image,
     ImageMetadata,
@@ -18,15 +15,17 @@ from robot_interface.models.inspection.inspection import (
     ThermalVideoMetadata,
     Video,
     VideoMetadata,
+    CO2Measurement,
 )
 from robot_interface.models.mission.task import (
     RecordAudio,
-    TakeGasMeasurement,
     TakeImage,
     TakeThermalImage,
     TakeThermalVideo,
     TakeVideo,
+    TakeCO2Measurement,
 )
+from typing import Union
 
 from isar_robot import telemetry
 
@@ -114,23 +113,23 @@ def create_audio(task_actions: RecordAudio):
     return Audio(metadata=audio_metadata, id=task_actions.inspection_id, data=data)
 
 
-def create_gas_measurement(task_actions: TakeGasMeasurement):
+def create_co2_measurement(task_actions: TakeCO2Measurement):
     now: datetime = datetime.now(timezone.utc)
     gas_measurement_metadata: GasMeasurementMetadata = GasMeasurementMetadata(
         start_time=now,
         pose=telemetry.get_pose(),
-        file_type="wav",
+        file_type="not_a_file",
     )
     gas_measurement_metadata.tag_id = task_actions.tag_id
     gas_measurement_metadata.inspection_description = (
         task_actions.inspection_description
     )
 
-    filepath: Path = random.choice(list(example_thermal_videos.iterdir()))
-    data = _read_data_from_file(filepath)
-
-    return GasMeasurement(
-        metadata=gas_measurement_metadata, id=task_actions.inspection_id, data=data
+    return CO2Measurement(
+        metadata=gas_measurement_metadata,
+        id=task_actions.inspection_id,
+        value=random.uniform(0, 5),
+        unit="% v/v",
     )
 
 
