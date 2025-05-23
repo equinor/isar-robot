12578df78446ef92287337f73d94f1b1b1471e50
Thomas Sundvoll <35451859+tsundvoll@users.noreply.github.com>
2025-05-23 13:24:17 +0200

    Refactor return to home check
    by removing uneccessary utilities file

--- src/isar_robot/utilities.py
+++
@@ -1,6 +0,0 @@
-from robot_interface.models.mission.task import Task
-from robot_interface.models.mission.task import ReturnToHome
-
-
-def is_return_to_home_task(task: Task):
-    return isinstance(task, ReturnToHome)
