75f1ad4553af3d05f06f2032a9d6e18eddc3a235
Eddasol <esole@equinor.com>
2026-02-17 13:25:11 +0100

    Add missing parameter in tests

--- tests/test_inspections.py
+++ tests/test_inspections.py
@@ -1,4 +1,4 @@
-from alitra import Frame, Position
+from alitra import Frame, Position, Pose, Orientation
 from robot_interface.models.mission.task import (
     RecordAudio,
     TakeImage,
@@ -8,12 +8,17 @@ from robot_interface.models.mission.task import (
 
 from isar_robot import inspections, telemetry
 
+robot_pose = Pose(
+    Position(0, 0, 0, Frame("asset")),
+    Orientation(x=0, y=0, z=0, w=1, frame=Frame("asset")),
+    Frame("asset"),
+)
 target = Position(x=0, y=0, z=0, frame=Frame("robot"))
 telemetryModule = telemetry.Telemetry()
 
 
 def test_create_image() -> None:
-    task_actions = TakeImage(target=target)
+    task_actions = TakeImage(target=target, robot_pose=robot_pose)
 
     inspection_image = inspections.create_image(task_actions, telemetryModule)
 
@@ -21,7 +26,7 @@ def test_create_image() -> None:
 
 
 def test_create_thermal_image() -> None:
-    task_actions = TakeThermalImage(target=target)
+    task_actions = TakeThermalImage(target=target, robot_pose=robot_pose)
 
     inspection_image = inspections.create_thermal_image(task_actions, telemetryModule)
 
@@ -29,7 +34,7 @@ def test_create_thermal_image() -> None:
 
 
 def test_create_video() -> None:
-    task_actions = TakeImage(target=target)
+    task_actions = TakeImage(target=target, robot_pose=robot_pose)
 
     inspection_video = inspections.create_video(task_actions, telemetryModule)
 
@@ -37,7 +42,7 @@ def test_create_video() -> None:
 
 
 def test_create_thermal_video() -> None:
-    task_actions = TakeThermalVideo(target=target, duration=10)
+    task_actions = TakeThermalVideo(target=target, duration=10, robot_pose=robot_pose)
 
     inspection_video = inspections.create_thermal_video(task_actions, telemetryModule)
 
@@ -46,7 +51,7 @@ def test_create_thermal_video() -> None:
 
 
 def test_create_audio() -> None:
-    task_actions = RecordAudio(target=target, duration=10)
+    task_actions = RecordAudio(target=target, duration=10, robot_pose=robot_pose)
 
     inspection_recording = inspections.create_audio(task_actions, telemetryModule)
 
